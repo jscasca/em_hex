34f62c4c91b786bb9334d5237e6ebcb475fbd18e
jscasca <jscasca@gmail.com>
2020-04-12 18:02:56 +1000

    Terrain script

---
+++ EM Hex basics/Assets/GamePlayerSrc.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GamePlayerSrc : MonoBehaviour
+{
+    public Grid grid;
+
+    public Tilemap terrainMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(string.Format("Co-ords of mouse is [X: {0} Y: {0}]", pos.x, pos.y));
+            Vector3Int coord = grid.WorldToCell(pos);
+            Debug.Log("Click on: " + coord.ToString());
+
+            // if (g == null) {
+            //     Debug.Log("No inst object");
+            //     // Open the menu for action? Check if the thing has the player etc...
+            // } else {
+            //     Debug.Log("Inst: " + g.ToString());
+            // }
+
+            // check if there is a tile there?
+        }
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            //
+            if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                Debug.Log("Zoom");
+                Zoom(true);
+            } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                Debug.Log("swosh");
+                Zoom(false);
+            }
+        } else {
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            // move view
+                Camera.main.transform.Translate (Input.GetAxisRaw("Horizontal")*2,Input.GetAxisRaw("Vertical")*2, 0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            Debug.Log("Zoom");
+            Zoom(true);
+        }
+        if (Input.GetKeyDown(KeyCode.X)) {
+            Debug.Log("swosh");
+            Zoom(false);
+        }
+    }
+
+    void Zoom(bool zoomIn) {
+        // Camera.main.transform.Translate(0, 0, zoomIn ? 1 : -1);
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, Camera.main.orthographicSize + (zoomIn? 1: -1), 2);
+    }
+}
---
+++ EM Hex basics/Assets/TerrainEngineSrc.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TerrainEngineSrc : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    public Tile plains;
+    public Tile swamps;
+    public Tile forests;
+    public Tile mountains;
+    public Tile waters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake() {
+        Debug.Log("Init terrain tilemap");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                //
+                Vector3Int pos = new Vector3Int(i, j, 0);
+                Tile t = Instantiate(plains);
+                tilemap.SetTile(pos, t);
+            }
+        }
+    }
+}
